2913d90e1d063b6e5273bf864021057d70505990
Kubix1311 <kubaherobrineyt@gmail.com>
2024-12-29 22:38:23 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,48 +1,29 @@
 <?php
-
-$servername = "125.0.0.1";
-$username = "root";
-$password = ""; 
+// Połączenie z bazą danych
+$servername = "localhost";
+$username = "twoja_nazwa_użytkownika";
+$password = "twoje_hasło";
 $dbname = "logowanie_1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
+// Sprawdzenie połączenia
 if ($conn->connect_error) {
-    die("Błąd połączenia: " . $conn->connect_error);
+  die("Połączenie nieudane: " . $conn->connect_error);
 }
 
+// Pobranie danych z formularza
+$username = $_POST["username"];
+$password = $_POST["password"];
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Przygotowanie zapytania SQL
+$sql = "INSERT INTO logowanie_1 (username, password) VALUES ('$username', '$password')";
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-
-    $sql = "INSERT INTO logowanie_1 (username, password) VALUES ('$username', '$password')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Rejestracja zakończona sukcesem!";
-    } else {
-        echo "Błąd: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-
-$sql = "SELECT username FROM logowanie_1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "<h2>Lista zarejestrowanych użytkowników:</h2>";
-    echo "<ul>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row["username"] . "</li>";
-    }
-    echo "</ul>";
+// Wykonanie zapytania
+if ($conn->query($sql) === TRUE) {
+  echo "Zalogowano pomyślnie!";
 } else {
-    echo "Brak zarejestrowanych użytkowników.";
+  echo "Błąd: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();

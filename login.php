97095963b92b871e482b9803477ab9d6094ea273
Kubix1311 <kubaherobrineyt@gmail.com>
2024-12-29 11:34:05 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
-// Połączenie z bazą danych
-$servername = "127.0.0.1";
-$username = "root"; // Zmień na swoją nazwę użytkownika
-$password = ""; // Zmień na swoje hasło
+
+$servername = "125.0.0.1";
+$username = "root";
+$password = ""; 
 $dbname = "logowanie_1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,16 +12,16 @@ if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
 
-// Sprawdzenie, czy formularz został wysłany
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pobranie danych z formularza
+
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Szyfrowanie hasła
+
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Wstawienie danych do bazy
+
     $sql = "INSERT INTO logowanie_1 (username, password) VALUES ('$username', '$password')";
     if ($conn->query($sql) === TRUE) {
         echo "Rejestracja zakończona sukcesem!";
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Wyświetlenie zarejestrowanych użytkowników
+
 $sql = "SELECT username FROM logowanie_1";
 $result = $conn->query($sql);
 
